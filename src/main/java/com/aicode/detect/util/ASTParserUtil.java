f8af6d3fcc6b1675cc69ee5d2a772ad833a1167b
Harisankar <harisankar2206@gmail.com>
2026-01-14 14:07:35 +0530

    Create ASTParserUtil.java

---
+++ src/main/java/com/aicode/detect/util/ASTParserUtil.java
@@ -0,0 +1,10 @@
+package com.aicode.detect.util;
+
+public class ASTParserUtil {
+
+    public static Object parse(String code, String language) {
+        // Convert code to language-neutral AST
+        // This is a placeholder; in reality you'd use language-specific parser libraries
+        return new Object();
+    }
+}
