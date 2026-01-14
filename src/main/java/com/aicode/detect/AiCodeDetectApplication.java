b7639f38c155edd7a824ca1ce41a29212541dd76
Harisankar <harisankar2206@gmail.com>
2026-01-14 14:18:49 +0530

    Create AiCodeDetectApplication.java

---
+++ src/main/java/com/aicode/detect/AiCodeDetectApplication.java
@@ -0,0 +1,11 @@
+package com.aicode.detect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCodeDetectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AiCodeDetectApplication.class, args);
+    }
+}
