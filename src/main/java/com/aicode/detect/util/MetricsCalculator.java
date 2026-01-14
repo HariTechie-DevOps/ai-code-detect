b0bdfa320e04099845770f73ada011f709421505
Harisankar <harisankar2206@gmail.com>
2026-01-14 14:08:27 +0530

    Add MetricsCalculator class with placeholder methods

---
+++ src/main/java/com/aicode/detect/util/MetricsCalculator.java
@@ -0,0 +1,12 @@
+package com.aicode.detect.util;
+
+public class MetricsCalculator {
+
+    public static double calculateLogicDensity(Object ast) {
+        return 0.25; // Placeholder logic
+    }
+
+    public static int calculateAbstractionJumps(Object ast) {
+        return 0; // Placeholder
+    }
+}
